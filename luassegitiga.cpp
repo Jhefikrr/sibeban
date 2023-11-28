e6692393c929a2c693b6e29e01ff529b3a2a5ff9
zyy <145952055+Jhefikrr@users.noreply.github.com>
2023-11-28 19:23:30 +0800

    Update luassegitiga.cpp

--- luassegitiga.cpp
+++ luassegitiga.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void segitiga(){
+int main(){
+    int tinggi, alas, hasil; 
     cout << endl;
     cout << "==== MENGHITUNG LUAS SEGITIGA =====" << endl;
-    
+    cout << "masukkan nilai tinggi : ";
+    cin >> tinggi;
+    cout << "masukkan nilai jari-jari :";
+    cin >> alas;
+    hasil = tinggi * (alas/2);
+    cout << "hasil dari operasi adalah : " << hasil << endl;
 }
